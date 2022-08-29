9a95a9ab7c401ee9c2e1432d5f902e30ccea9f69
unknown <ben.gabay38@gmail.com>
2022-08-29 17:48:08 +0300

    rename str assembly func to str instructions in usage

--- src/graph_draw.py
+++ src/graph_draw.py
@@ -1,7 +1,7 @@
 import networkx as nx
 from matplotlib import pyplot as plt
 
-from src.str_assembly import str_assembly, str_jump_branch_taken
+from src.str_assembly import str_instructions, str_jump_branch_taken
 
 NODE_SIZE = 1000
 NODE_SHAPE = "s"
@@ -11,7 +11,7 @@ FILE_PATH = "../path.png"
 
 def draw_nodes(graph, pos):
     node_labels = nx.get_node_attributes(graph, "instructions")
-    node_labels = {node: str_assembly(instructions) for node, instructions in node_labels.items()}
+    node_labels = {node: str_instructions(instructions) for node, instructions in node_labels.items()}
     nx.draw_networkx_nodes(graph, pos, node_shape=NODE_SHAPE, node_size=NODE_SIZE)
     nx.draw_networkx_labels(graph, pos, labels=node_labels)
 
