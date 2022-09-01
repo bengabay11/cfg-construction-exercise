407f5b42d2360b2b2f742c5163498c66748a6356
unknown <ben.gabay38@gmail.com>
2022-09-01 22:25:47 +0300

    draw nodes from top to the bottom

--- src/graph_draw.py
+++ src/graph_draw.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle
 
 from src.str_assembly import str_instructions, str_jump_branch_taken, str_jump_branch_not_taken
 
@@ -21,13 +22,14 @@ def draw_edges(graph, pos):
     edge_jump_labels = {edge: str_jump_branch_taken(jump) for edge, jump in edge_jump_labels.items()}
     edge_no_jump_labels = nx.get_edge_attributes(graph, "no_jump")
     edge_no_jump_labels = {edge: str_jump_branch_not_taken(jump) for edge, jump in edge_no_jump_labels.items()}
-    nx.draw_networkx_edges(graph, pos, arrows=True, node_size=NODE_SIZE, node_shape=NODE_SHAPE)
     nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_jump_labels)
     nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_no_jump_labels)
+    nx.draw_networkx_edges(graph, pos, arrows=True, node_size=NODE_SIZE, node_shape=NODE_SHAPE)
 
 
 def draw_graph(graph):
-    pos = nx.spring_layout(graph)
+    # pos = nx.spring_layout(graph)
+    pos = nx.multipartite_layout(graph, subset_key="level", align="horizontal")
     draw_nodes(graph, pos)
     draw_edges(graph, pos)
     plt.savefig(FILE_PATH)
