0969d7b55f9169b8dfedd947e71cb76b07c1d373
בן גבאי <ben.gabay38@gmail.com>
2022-09-02 03:07:48 +0300

    remove code init examples from main

--- src/main.py
+++ src/main.py
@@ -1,40 +1,11 @@
+from typing import List
+
 from src.assembly_validtor import validate_assembly_code
 from src.cfg_builder import create_cfg
-from src.cfg_construction import Assignment, Call, Jump, Var, Expression, Operation
 from src.graph_draw import draw_graph
 
 
-def main():
-    base = Var("base")
-    counter = Var("counter")
-    output = Var("output")
-    # code = [
-    #     Assignment(base, 2),  # 0
-    #     Assignment(counter, 5),  # 1
-    #     Assignment(output, 1),  # 2
-    #     # Loop start
-    #     Assignment(output, Expression(output, base, Operation.MUL)),  # 3
-    #     Assignment(counter, Expression(counter, 1, Operation.SUB)),  # 4
-    #     Jump(3, counter),  # 5
-    #     Call("print", [output])  # 6
-    # ]
-    code = [
-        Assignment(counter, 6),  # 0
-        Assignment(output, 1),  # 1
-        Jump(6, counter),  # 2
-        "Hello",
-        Call("insmod", [6]),  # 3
-        Call("lsmod", [6]),  # 4
-        Jump(9, counter),  # 5
-        Call("print1", [output]),  # 6
-        Call("print2", [output]),  # 7
-        Call("print3", [output]),  # 8
-        Call("exit", [output])  # 9
-    ]
-    validate_assembly_code(code)
-    graph = create_cfg(code)
+def build_cfg(instructions: List) -> None:
+    validate_assembly_code(instructions)
+    graph = create_cfg(instructions)
     draw_graph(graph)
-
-
-if __name__ == '__main__':
-    main()
