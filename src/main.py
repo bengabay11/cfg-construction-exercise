862fc22d988d3ea147965b8b51582e95352711b3
נועה גבאי <ben.gabay38@gmail.com>
2022-09-07 00:25:31 +0300

    add usage of cfg builder class to main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from typing import List
 
-from src.assembly_validtor import validate_assembly_code
-from src.cfg_builder import create_cfg
+from src.logic.assembly_validtor import validate_assembly_code
+from src.logic.cfg_builder import CFGBuilder
 from src.ui.graph_draw import draw_graph
 
 
 def build_cfg(instructions: List) -> None:
     validate_assembly_code(instructions)
-    graph = create_cfg(instructions)
-    draw_graph(graph)
+    cfg_builder = CFGBuilder("CFG", instructions)
+    cfg = cfg_builder.build()
+    draw_graph(cfg.graph)
