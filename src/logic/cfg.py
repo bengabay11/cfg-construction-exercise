701dafe83d3d9589555efdfdf4199c654a45a7e6
נועה גבאי <ben.gabay38@gmail.com>
2022-09-07 00:27:04 +0300

    move cfg builder functions logic into classes

---
+++ src/logic/cfg.py
@@ -0,0 +1,24 @@
+import networkx as nx
+
+from src.models.basic_block import BasicBlock
+from src.models.link import Link
+
+
+class CFG(object):
+    def __init__(self, name: str, basic_blocks=None, links=None):
+        if links is None:
+            links = []
+        if basic_blocks is None:
+            basic_blocks = []
+        self.name = name
+        self.graph = nx.DiGraph()
+        self.basic_blocks = basic_blocks
+        self.links = links
+
+    def add_basic_block(self, basic_block: BasicBlock):
+        self.basic_blocks.append(basic_block)
+        self.graph.add_node(basic_block.index, instructions=basic_block.instructions)
+
+    def add_link(self, link: Link):
+        self.links.append(link)
+        self.graph.add_edge(link.source, link.target, condition=link.condition, branch_taken=link.branch_taken)
---
+++ src/logic/cfg_builder.py
@@ -0,0 +1,61 @@
+from typing import List
+
+from src.models.basic_block import BasicBlock
+from src.logic.cfg import CFG
+from src.cfg_construction import Jump
+from src.models.link import Link
+
+
+class CFGBuilder(object):
+    def __init__(self, graph_name: str, code: List[any]) -> None:
+        self.code = code
+        self.cfg = CFG(graph_name)
+
+    def build(self) -> CFG:
+        leader_indexes = self.get_leader_indexes()
+        self.add_basic_blocks_to_cfg(leader_indexes)
+        self.add_links_to_cfg(leader_indexes)
+        return self.cfg
+
+    def get_leader_indexes(self) -> List[int]:
+        if not self.code:
+            return []
+        leader_indexes = {0}
+        for index, instruction in enumerate(self.code):
+            if isinstance(instruction, Jump):
+                if instruction.condition and index < len(self.code) - 1:
+                    leader_indexes.add(index + 1)
+                leader_indexes.add(instruction.target)
+        return sorted(leader_indexes)
+
+    def add_basic_blocks_to_cfg(self, leader_indexes: List) -> None:
+        basic_blocks_ranges = zip(leader_indexes, leader_indexes[1:] + [None])
+        for index, (start, end) in enumerate(basic_blocks_ranges):
+            basic_block = BasicBlock(index, self.code[start:end])
+            self.cfg.add_basic_block(basic_block)
+
+    def create_conditional_jump_links(self, jump: Jump, current_basic_block_index: int, target_basic_block_index: int):
+        branch_taken_link = Link(current_basic_block_index, target_basic_block_index, jump.condition, True)
+        self.cfg.add_link(branch_taken_link)
+        branch_not_taken_link = Link(current_basic_block_index, current_basic_block_index + 1, jump.condition, False)
+        self.cfg.add_link(branch_not_taken_link)
+
+    def create_unconditional_jump_link(self, current_basic_block_index: int, target_basic_block_index: int):
+        unconditional_link = Link(current_basic_block_index, target_basic_block_index)
+        self.cfg.add_link(unconditional_link)
+
+    def create_jump_links(self, leader_indexes: List[int], current_basic_block_index: int, jump: Jump) -> None:
+        if jump.target in leader_indexes:
+            target_basic_block_index = leader_indexes.index(jump.target)
+            if jump.condition and current_basic_block_index < len(self.cfg.basic_blocks) - 1:
+                self.create_conditional_jump_links(jump, current_basic_block_index, target_basic_block_index)
+            else:
+                self.create_unconditional_jump_link(current_basic_block_index, target_basic_block_index)
+
+    def add_links_to_cfg(self, leader_indexes: List[int]) -> None:
+        for basic_block in self.cfg.basic_blocks:
+            if isinstance(basic_block.instructions[-1], Jump):
+                self.create_jump_links(leader_indexes, basic_block.index, basic_block.instructions[-1])
+            elif basic_block.index < len(self.cfg.basic_blocks) - 1:
+                link = Link(basic_block.index, basic_block.index + 1)
+                self.cfg.add_link(link)
