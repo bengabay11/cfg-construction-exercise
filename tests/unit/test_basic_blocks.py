0b28f5c24cbc9db6f8fabc6f3f0f644a918329de
בן גבאי <ben.gabay38@gmail.com>
2022-09-09 10:10:23 +0300

    fix the old usage in class AssemblyValidator to work with delegate in tests

--- tests/unit/test_basic_blocks.py
+++ tests/unit/test_basic_blocks.py
@@ -3,13 +3,14 @@ from typing import List
 import pytest
 
 from src.cfg_construction import Assignment, Var, Call
+from src.logic.assembly_validtor import validate_assembly_code
 from src.logic.cfg_builder import CFGBuilder
 from src.models.basic_block import BasicBlock
 
 
 @pytest.mark.skip
 def test_basic_blocks(instructions: List, leader_indexes: List[int], expected_basic_blocks: List[BasicBlock]):
-    cfg_builder = CFGBuilder("test_cfg", instructions)
+    cfg_builder = CFGBuilder("test_cfg", instructions, validate_assembly_code)
     cfg_builder.add_basic_blocks_to_cfg(leader_indexes)
     assert cfg_builder.cfg.basic_blocks == expected_basic_blocks
     assert len(cfg_builder.cfg.graph.nodes) == len(expected_basic_blocks)
--- tests/unit/test_leader_indexes.py
+++ tests/unit/test_leader_indexes.py
@@ -3,31 +3,27 @@ from typing import List
 import pytest
 
 from src.cfg_construction import Var, Assignment, Call, Jump
-from src.logic.assembly_validtor import AssemblyValidator
+from src.logic.assembly_validtor import validate_assembly_code
 from src.logic.cfg_builder import CFGBuilder
 
 
 @pytest.mark.skip
-def test_get_leader_indexes(code: List, expected_leader_indexes: List[int])-> None:
-    assembly_validator = AssemblyValidator(code)
-    cfg_builder = CFGBuilder("test_graph", code, assembly_validator)
+def test_get_leader_indexes(code: List, expected_leader_indexes: List[int]) -> None:
+    cfg_builder = CFGBuilder("test_graph", code, validate_assembly_code)
     leader_indexes = cfg_builder.get_leader_indexes()
-    assert leader_indexes == [0]
+    assert leader_indexes == expected_leader_indexes
+
 
 def test_get_leader_indexes_no_jumps(var: Var, exit_instruction: Call) -> None:
     code = [
         Assignment(var, 1),
         exit_instruction
     ]
-    cfg_builder = CFGBuilder("test_graph", code)
-    leader_indexes = cfg_builder.get_leader_indexes()
-    assert leader_indexes == [0]
+    test_get_leader_indexes(code, [0])
 
 
 def test_get_leader_indexes_empty_code():
-    cfg_builder = CFGBuilder("test_graph", [])
-    leader_indexes = cfg_builder.get_leader_indexes()
-    assert leader_indexes == []
+    test_get_leader_indexes([], [])
 
 
 def test_get_leader_indexes_jump(var: Var, exit_instruction: Call):
@@ -38,40 +34,32 @@ def test_get_leader_indexes_jump(var: Var, exit_instruction: Call):
         Call("print", [var]),
         exit_instruction
     ]
-    cfg_builder = CFGBuilder("test_graph", code)
-    leader_indexes = cfg_builder.get_leader_indexes()
-    assert leader_indexes == [0, 2, 4]
+    test_get_leader_indexes(code, [0, 2, 4])
 
 
-def test_get_leader_indexes_first_line_target(var: Var, exit_instruction: Call):
+def test_get_leader_indexes_first_line_target(var: Var, exit_instruction: Call) -> None:
     code = [
         Assignment(var, 1),
         Jump(0, var),
         exit_instruction
     ]
-    cfg_builder = CFGBuilder("test_graph", code)
-    leader_indexes = cfg_builder.get_leader_indexes()
-    assert leader_indexes == [0, 2]
+    test_get_leader_indexes(code, [0, 2])
 
 
-def test_get_leader_indexes_jump_last_instruction(var: Var):
+def test_get_leader_indexes_jump_last_instruction(var: Var) -> None:
     code = [
         Assignment(var, 1),
         Call("print", [var]),
         Jump(1, var),
     ]
-    cfg_builder = CFGBuilder("test_graph", code)
-    leader_indexes = cfg_builder.get_leader_indexes()
-    assert leader_indexes == [0, 1]
+    test_get_leader_indexes(code, [0, 1])
 
 
-def test_get_leader_indexes_unconditional_jump(var: Var, exit_instruction: Call):
+def test_get_leader_indexes_unconditional_jump(var: Var, exit_instruction: Call) -> None:
     code = [
         Assignment(var, 1),
         Jump(3, None),
         Call("print", [var]),
         exit_instruction
     ]
-    cfg_builder = CFGBuilder("test_graph", code)
-    leader_indexes = cfg_builder.get_leader_indexes()
-    assert leader_indexes == [0, 3]
+    test_get_leader_indexes(code, [0, 3])
