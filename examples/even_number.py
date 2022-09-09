31cac8a97ff6d7f59dc4dc615940a759ba69aacf
unknown <ben.gabay38@gmail.com>
2022-09-10 00:51:23 +0300

    fix assignment in even number example

--- examples/even_number.py
+++ examples/even_number.py
@@ -1,18 +1,19 @@
 from typing import List
 
-from src.cfg_construction import Var, Assignment, Jump, Call, Value
+from src.cfg_construction import Var, Assignment, Jump, Call, Value, Expression, Operation
 from src.main import build_cfg
 
 
 def build_even_number_code() -> List:
+    num = Var("num")
     result = Var("result")
     output = Var("output")
     return [
-        Assignment(result, 6 % 2),  # 0
+        Assignment(result, Expression(num, 2, Operation.DIV)),  # 0
         Jump(4, result),  # 1
-        Call("even_number", [6]),  # 2
+        Call("even_number", [num]),  # 2
         Jump(5, None),  # 3
-        Call("odd_number", [6]),  # 4
+        Call("odd_number", [num]),  # 4
         Call("exit", [output])  # 5
     ]
 
